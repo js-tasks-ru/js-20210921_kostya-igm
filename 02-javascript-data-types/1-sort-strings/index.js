7e7bdd918d1e78719491abf0e4684cfc124f7cf3
Konstantin Biriukov <kostya-leon@mail.ru>
2021-09-25 08:27:15 -0600

    sort array by localeCompare

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  if (param === "desc") {
+    return arr.slice().sort((a, b) => {
+      return b.localeCompare(a, "ru-u-kf-upper")
+    })
+  } else {
+    return arr.slice().sort((a, b) => {
+      return a.localeCompare(b, "ru-u-kf-upper")
+    })
+  }
 }

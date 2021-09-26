7e8b0c3dfcdd7bab6b01e50c6eb91b4cbf2d7745
Konstantin Biriukov <kostya-leon@mail.ru>
2021-09-25 22:19:05 -0600

    omit function implementation

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,6 +4,21 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
 
+export const omit = (obj, ...fields) => {
+  const newObj = {};
+  for (let [key, value] of Object.entries(obj)) {
+    let isFieldExist = false;
+
+    // "for" loop is used instead of forEach method,
+    // because the ability to interrupt a loop is desired in this scenario.
+    for (let i = 0; i < fields.length; i++) {
+      if (key === fields[i]) {
+        isFieldExist = true;
+        break; // there is no need to continue if the field exists
+      }
+    }
+    if (!isFieldExist) newObj[key] = value;
+  }
+  return newObj;
 };

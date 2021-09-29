c3c0da5b8666f06781e56495ef37e237d8ac6839
Konstantin Biriukov <kostya-leon@mail.ru>
2021-09-28 20:48:41 -0600

    createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,27 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const keysArray = path.split(".")
+  return function (obj) {
+    return getKey(obj, keysArray);
+  }
+}
 
+function getKey(obj, keysArray) {
+  let valueFound = ""
+
+  function deepIterate(obj) {
+    for (const [key, value] of Object.entries(obj)) {
+      if (keysArray.includes(key)) {
+        if (typeof value === "object") {
+          deepIterate(value)
+        } else {
+          valueFound = value;
+        }
+      }
+    }
+  }
+
+  deepIterate(obj)
+  return (valueFound) ? valueFound : undefined
 }

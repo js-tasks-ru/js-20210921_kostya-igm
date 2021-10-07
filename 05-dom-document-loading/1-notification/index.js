8cc186f72c83ec7207134216a332e2c0985cfe8f
Konstantin Biriukov <kostya-leon@mail.ru>
2021-10-07 17:37:54 -0500

    lecture 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,77 @@
 export default class NotificationMessage {
+  static store; // for Singleton pattern, to have only one instance of the class
+  static timerId;
 
+  constructor(message = "", {duration = 0, type = ""} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  get template() {
+    return `
+     <div class="notification" style="--value:${this.duration}ms">
+     <div class="timer"></div>
+       <div class="inner-wrapper">
+         <div class="notification-header">
+                Notification
+          </div>
+          <div class="notification-body">
+               ${this.message} ${Math.random()}
+          </div>
+        </div>
+      </div>
+   `;
+  }
+
+  get store() {
+    return NotificationMessage.store;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  show() {
+    if (NotificationMessage.store) {
+      document.body.querySelector(".notification-body").textContent = `${this.message} ${Math.random()}`;
+    } else {
+      NotificationMessage.store = this.template;
+      document.body.insertAdjacentHTML("beforeend", this.template);
+      this.element = document.body.querySelector(".notification");
+      this.element.classList.add(this.type);
+
+      setTimeout(() => {
+        this.remove(this.element);
+        NotificationMessage.store = null;
+      }, this.duration);
+    }
+    this.subElements = this.getSubElements(this.element);
+
+  }
+
+  remove(element) {
+    element.remove();
+  }
+
+  destroy() {
+    this.remove(this.element);
+    this.element = null;
+    this.subElements = {};
+  }
+
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,112 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  static sortedData;
 
+  constructor(headerConfig = [], data = []) {
+    SortableTable.sortedData = data;
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.order = "asc"
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+    <div data-element="productsContainer" class="products-list__container">
+      <div class="sortable-table">
+        <div data-element="header" class="sortable-table__header sortable-table__row">
+          <div class="sortable-table__cell" data-id="${this.headerConfig[0].id}" data-sortable="${this.headerConfig[0].sortable}" data-order="${this.order}">
+            <span>${this.headerConfig[0].title}</span>
+          </div>
+          <div class="sortable-table__cell" data-id="${this.headerConfig[1].id}" data-sortable="${this.headerConfig[1].sortable}" data-order="${this.order}">
+            <span>${this.headerConfig[1].title}</span>
+            <span data-element="arrow" class="sortable-table__sort-arrow">
+              <span class="sort-arrow"></span>
+            </span>
+          </div>
+          <div class="sortable-table__cell" data-id="${this.headerConfig[2].id}" data-sortable="${this.headerConfig[2].sortable}" data-order="${this.order}">
+            <span>${this.headerConfig[2].title}</span>
+          </div>
+          <div class="sortable-table__cell" data-id="${this.headerConfig[3].id}" data-sortable="${this.headerConfig[3].sortable}" data-order="${this.order}">
+            <span>${this.headerConfig[3].title}</span>
+          </div>
+          <div class="sortable-table__cell" data-id="${this.headerConfig[4].id}" data-sortable="${this.headerConfig[4].sortable}" data-order="${this.order}">
+            <span>${this.headerConfig[4].title}</span>
+          </div>
+        </div>
+        <div data-element="body" class="sortable-table__body">
+
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+  getBody(i, image) {
+    return `
+          <a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
+          ${image}
+            <div class="sortable-table__cell">${SortableTable.sortedData[i].title}</div>
+            <div class="sortable-table__cell">${SortableTable.sortedData[i].quantity}</div>
+            <div class="sortable-table__cell">${SortableTable.sortedData[i].price}</div>
+            <div class="sortable-table__cell">${SortableTable.sortedData[i].sales}</div>
+          </a>
+    `
+  }
+
+  render() {
+    const element = document.createElement('div'); // (*)
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+
+    const root = document.getElementById("root");
+    root.innerHTML = this.getTemplate();
+
+    const body = document.body.querySelector(".sortable-table__body");
+    for (let i = 0; i < SortableTable.sortedData.length; i++) {
+      const image = this.headerConfig[0].template(this.data[i].images)
+      body.insertAdjacentHTML('beforebegin', this.getBody(i, image));
+    }
+    this.subElements = this.getSubElements(this.element);
+  }
+
+  /**
+   * sortStrings - sorts array of string by two criteria "asc" or "desc"
+   * @param {string} fieldValue - value property in 'option' tag of the 'select' tag
+   * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
+   * @returns {string[]}
+   */
+  sort(fieldValue, param = 'asc') {
+    (param === "desc") ?
+      SortableTable.sortedData.sort((a, b) => this.sortHelper(a[fieldValue], b[fieldValue])) :
+      SortableTable.sortedData.sort((a, b) => this.sortHelper(b[fieldValue], a[fieldValue])) // "asc"
+
+    this.order = param;
+    this.render();
+    return SortableTable.sortedData;
+  }
+
+  sortHelper(a, b) {
+    return String(b).localeCompare(String(a), "ru-u-kf-upper")
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
   }
 }
 
